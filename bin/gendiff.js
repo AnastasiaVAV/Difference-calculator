70f01d48669cc99e7ea6ff385f2267a758ac2884
nastya volkova <derric41@mail.ru>
2025-04-09 18:59:18 +0400

    4. Comparing Flat Files (JSON)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,7 @@
 import { program } from 'commander';
 // import app from '../src/index.js';
 import parse from '../src/parse.js';
+import _ from 'lodash';
 
 program
   .name('gendiff')
@@ -13,6 +14,32 @@ program
   .action((filepath1, filepath2) => {
     const parseFile1 = parse(filepath1);
     const parseFile2 = parse(filepath2);
+    
+    const gendiff = (file1, file2) => {
+      const keysFile1 = Object.keys(file1);
+      const keysFile2 = Object.keys(file2);
+      const sortedKeys = _.union(keysFile1, keysFile2).sort();
+
+      const diff = sortedKeys
+        .map((key) => {
+          const propertyFile1 = file1[key];
+          const propertyFile2 = file2[key];
+
+          if (!Object.hasOwn(parseFile1, key)) {
+            return `+ ${key}: ${propertyFile2}\n`;
+          } else if (!Object.hasOwn(parseFile2, key)) {
+            return `- ${key}: ${propertyFile1}\n`;
+          } else if (propertyFile1 === propertyFile2) {
+            return `  ${key}: ${propertyFile1}\n`;
+          } else {
+            return `- ${key}: ${propertyFile1}\n+ ${key}: ${propertyFile2}\n`;
+          }
+        })
+        .join('')
+        .trim();
+      return '{\n' + diff + '\n}';
+    };
+    console.log(gendiff(parseFile1, parseFile2));
   })
 
 program.parse(process.argv);
