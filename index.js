f8e9603be485cc194185ed4733f8d969f2999887
nastya volkova <derric41@mail.ru>
2025-04-15 09:56:14 +0400

    edit function "buildTree" in index.js

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 import _ from 'lodash';
 import parse from './src/parse.js';
 
+const buildTree = (obj1, obj2) => {
+  const allKeys = _.union(Object.keys(obj1), Object.keys(obj2));
+
+  return allKeys.reduce((acc, key) => {
+    const hasKey1 = Object.hasOwn(obj1, key);
+    const hasKey2 = Object.hasOwn(obj2, key);
+
+    if (!hasKey1 || !hasKey2) {
+      acc[key] = !hasKey1
+        ? { name: key, type: 'added', value: obj2[key] }
+        : { name: key, type: 'removed', value: obj1[key] };
+    } else {
+      const isEquality = obj1[key] === obj2[key];
+      acc[key] = isEquality
+        ? { name: key, type: 'unchanged', value: obj1[key] }
+        : {
+          name: key,
+          type: 'changed',
+          valueOld: obj1[key],
+          valueNew: obj2[key],
+        };
+    }
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      acc[key] = { name: key, type: 'unchanged', value: buildTree(obj1[key], obj2[key]) };
+    }
+    return acc;
+  }, {});
+};
+
 const sortedObject = (obj) => {
   if (!_.isObject(obj) || obj === null) {
     return obj;
@@ -11,37 +40,6 @@ const sortedObject = (obj) => {
       acc[key] = sortedObject(obj[key]);
       return acc;
     }, {});
-};
-
-const buildTree = (obj1, obj2) => {
-  const allKeys = _.union(Object.keys(obj1), Object.keys(obj2));
-
-  return allKeys.reduce((acc, key) => {
-    const hasKey1 = Object.hasOwn(obj1, key);
-    const hasKey2 = Object.hasOwn(obj2, key);
-
-    if (!hasKey1) {
-      acc[key] = { name: key, type: 'added', value: obj2[key] };
-    } else if (!hasKey2) {
-      acc[key] = { name: key, type: 'removed', value: obj1[key] };
-    } else if (hasKey1 && hasKey2) {
-      const isEquality = obj1[key] === obj2[key];
-      if (isEquality) {
-        acc[key] = { name: key, type: 'unchanged', value: obj1[key] };
-      } else {
-        acc[key] = {
-          name: key,
-          type: 'changed',
-          valueOld: obj1[key],
-          valueNew: obj2[key],
-        };
-      }
-    }
-    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      acc[key] = { name: key, type: 'unchanged', value: buildTree(obj1[key], obj2[key]) };
-    }
-    return acc;
-  }, {});
 };
 
 const gendiff = (filepath1, filepath2) => {
