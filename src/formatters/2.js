a6bb6ffe85ebd7b2f953843f6c0e9dabbc2412cb
nastya volkova <derric41@mail.ru>
2025-04-17 16:19:31 +0400

    edit formatters stylish and plain

--- src/formatters/2.js
+++ src/formatters/2.js
@@ -4,11 +4,12 @@ const replacer = ' ';
 const spacesCount = 4;
 const leftShift = 2;
 
-export default (tree) => {
+const stylish = (tree) => {
   const iter = (currentNode, depth) => {
     if (!_.isObject(currentNode)) {
       return `${currentNode}`;
     }
+
     const indentSize = (depth * spacesCount) - leftShift;
     const currentIndent = replacer.repeat(indentSize);
     const bracketIndent = indentSize > spacesCount
@@ -16,11 +17,15 @@ export default (tree) => {
       : '';
 
     if (!Array.isArray(currentNode)) {
-      const lines = Object
-        .entries(currentNode)
-        .map(([key, val]) => `${currentIndent}  ${key}: ${iter(val, depth + 1)}`);
+      const lines = Object.entries(currentNode).map(([key, val]) => {
+        if (_.isObject(val)) {
+          return `${currentIndent}${key}: ${iter(val, depth + 1)}`;
+        }
+        return `${currentIndent}${key}: ${val}`;
+      });
       return `{\n${lines.join('\n')}\n${bracketIndent}}`;
     }
+
     const lines = currentNode.map((node) => {
       switch (node.type) {
         case 'added':
@@ -30,14 +35,18 @@ export default (tree) => {
         case 'updated':
           return `${currentIndent}- ${node.name}: ${iter(node.oldValue, depth + 1)}\n${currentIndent}+ ${node.name}: ${iter(node.newValue, depth + 1)}`;
         case 'nested':
-          return `${currentIndent}  ${node.name}: ${iter(node.children, depth + 1)}`;
+          return `${currentIndent}  ${node.name}: {\n${iter(node.children, depth + 1)}\n${currentIndent}}`;
         case 'unchanged':
           return `${currentIndent}  ${node.name}: ${iter(node.value, depth + 1)}`;
         default:
           return '';
       }
     });
+
     return `{\n${lines.join('\n')}\n${bracketIndent}}`;
   };
+
   return iter(tree, 1);
 };
+
+export default stylish;
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -9,23 +9,47 @@ const formatValue = (value) => {
     : value;
 };
 
-const plain = (obj, path = '') => Object
-  .entries(obj)
-  .map(([key, node]) => {
-    const currentPath = path ? `${path}.${key}` : key;
-    const firstPart = `Property '${currentPath}' was ${node.type}`;
-    switch (node.type) {
-      case 'added':
-        return `${firstPart} with value: ${formatValue(node.value)}`;
-      case 'removed':
-        return `${firstPart}`;
-      case 'updated':
-        return `${firstPart}. From ${formatValue(node.valueOld)} to ${formatValue(node.valueNew)}`;
-      default:
-        return _.isObject(node.value) ? plain(node.value, currentPath) : '';
-    }
-  })
-  .filter(Boolean)
-  .join('\n');
+const plain = (tree) => {
+  const iter = (currentNode, path) => currentNode
+    .flatMap((node) => {
+      const currentPath = path ? `${path}.${node.name}` : node.name;
+      const firstPart = `Property '${currentPath}' was ${node.type}`;
+      switch (node.type) {
+        case 'added':
+          return `${firstPart} with value: ${formatValue(node.value)}`;
+        case 'removed':
+          return `${firstPart}`;
+        case 'updated':
+          return `${firstPart}. From ${formatValue(node.oldValue)} to ${formatValue(node.newValue)}`;
+        case 'nested':
+          return iter(node.children, currentPath);
+        default:
+          return [];
+      }
+    })
+    .join('\n');
+  return iter(tree, '');
+};
 
 export default plain;
+
+// const plain = (obj, path = '') => Object
+//   .entries(obj)
+//   .map(([key, node]) => {
+//     const currentPath = path ? `${path}.${key}` : key;
+//     const firstPart = `Property '${currentPath}' was ${node.type}`;
+//     switch (node.type) {
+//       case 'added':
+//         return `${firstPart} with value: ${formatValue(node.value)}`;
+//       case 'removed':
+//         return `${firstPart}`;
+//       case 'updated':
+//         return `${firstPart}. From ${formatValue(node.valueOld)} to ${formatValue(node.valueNew)}`;
+//       default:
+//         return _.isObject(node.value) ? plain(node.value, currentPath) : '';
+//     }
+//   })
+//   .filter(Boolean)
+//   .join('\n');
+
+// export default plain;
