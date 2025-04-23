fb889bdecce354d8e43aaa51f7f281faec0eedc3
nastya volkova <derric41@mail.ru>
2025-04-23 19:59:15 +0400

    edit formatter stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,33 +4,37 @@ const replacer = ' ';
 const spacesCount = 4;
 const leftShift = 2;
 
+const formatValue = (value, depth) => {
+  if (!_.isObject(value)) {
+    return `${value}`;
+  }
+  const indentSize = (depth * spacesCount) - leftShift;
+  const currentIndent = replacer.repeat(indentSize);
+  const bracketIndent = indentSize > spacesCount ? replacer.repeat((indentSize - spacesCount) + leftShift) : '';
+  const lines = Object
+    .entries(value)
+    .map(([key, val]) => `${currentIndent}  ${key}: ${formatValue(val, depth + 1)}`);
+  return `{\n${lines.join('\n')}\n${bracketIndent}}`;
+};
+
 export default (tree) => {
   const iter = (currentNode, depth) => {
-    if (!_.isObject(currentNode)) {
-      return `${currentNode}`;
-    }
     const indentSize = (depth * spacesCount) - leftShift;
     const currentIndent = replacer.repeat(indentSize);
     const bracketIndent = indentSize > spacesCount ? replacer.repeat((indentSize - spacesCount) + leftShift) : '';
 
-    if (!Array.isArray(currentNode)) {
-      const lines = Object
-        .entries(currentNode)
-        .map(([key, val]) => `${currentIndent}  ${key}: ${iter(val, depth + 1)}`);
-      return `{\n${lines.join('\n')}\n${bracketIndent}}`;
-    }
     const lines = currentNode.map((node) => {
       switch (node.type) {
         case 'added':
-          return `${currentIndent}+ ${node.name}: ${iter(node.value, depth + 1)}`;
+          return `${currentIndent}+ ${node.name}: ${formatValue(node.value, depth + 1)}`;
         case 'removed':
-          return `${currentIndent}- ${node.name}: ${iter(node.value, depth + 1)}`;
+          return `${currentIndent}- ${node.name}: ${formatValue(node.value, depth + 1)}`;
         case 'updated':
-          return `${currentIndent}- ${node.name}: ${iter(node.oldValue, depth + 1)}\n${currentIndent}+ ${node.name}: ${iter(node.newValue, depth + 1)}`;
+          return `${currentIndent}- ${node.name}: ${formatValue(node.oldValue, depth + 1)}\n${currentIndent}+ ${node.name}: ${formatValue(node.newValue, depth + 1)}`;
         case 'nested':
           return `${currentIndent}  ${node.name}: ${iter(node.children, depth + 1)}`;
         case 'unchanged':
-          return `${currentIndent}  ${node.name}: ${iter(node.value, depth + 1)}`;
+          return `${currentIndent}  ${node.name}: ${formatValue(node.value, depth + 1)}`;
         default:
           return '';
       }
